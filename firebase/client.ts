ec9c9d59de21357aaecc9281bae17fb1dfa9949b
Tanmay Sharma <tanmay.rsh@gmail.com>
2025-03-23 23:00:44 -0500

    Fixed exposed keys

--- firebase/client.ts
+++ firebase/client.ts
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyB9YYySoE9WxQseqSXbRSrC4-i4jlP6_QM",
-    authDomain: "prepyou-bd684.firebaseapp.com",
-    projectId: "prepyou-bd684",
-    storageBucket: "prepyou-bd684.firebasestorage.app",
-    messagingSenderId: "498386667804",
-    appId: "1:498386667804:web:aae2d9725020221cf892d9",
-    measurementId: "G-454KJV2JB4"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
